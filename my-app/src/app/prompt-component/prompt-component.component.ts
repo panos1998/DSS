66f639433fc6981672eb6b38fb2b73c2dd95c0d8
panos1998 <petridisp@ece.auth.gr>
2024-04-22 21:11:23 +0000

    2nd commit, next step messages

--- my-app/src/app/prompt-component/prompt-component.component.ts
+++ my-app/src/app/prompt-component/prompt-component.component.ts
@@ -1,24 +1,28 @@
 import { Component } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { SelectButtonModule } from 'primeng/selectbutton';
-import { FormsModule } from '@angular/forms'
+import { FormsModule,  ReactiveFormsModule, FormGroup, FormBuilder, FormControl } from '@angular/forms'
 import { ButtonModule } from 'primeng/button';
 
 
 @Component({
   selector: 'app-prompt-component',
   standalone: true,
-  imports: [SelectButtonModule, FormsModule, ButtonModule],
+  imports: [SelectButtonModule, FormsModule, ButtonModule, ReactiveFormsModule],
   templateUrl: './prompt-component.component.html',
   styleUrl: './prompt-component.component.css'
 })
 export class PromptComponentComponent {
   currentOptions: SelectItem[];
-  value: any
-  constructor () {
+  value: any;
+  formGroup!: FormGroup;
+    constructor () {
     this.currentOptions = [];
+    this.formGroup = new FormGroup({
+      value: new FormControl("")
+    })
     this.value = null;
-    this.currentOptions.push({label:'New York', value:'New York'});
+    this.currentOptions.push({label:'New York', value:'New Yorke'});
         this.currentOptions.push({label:'Rome', value:'Rome'});
         this.currentOptions.push({label:'London', value:'London'});
         this.currentOptions.push({label:'Istanbul', value:'Istanbul'});
