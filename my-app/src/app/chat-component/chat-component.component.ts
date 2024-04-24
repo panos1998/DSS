a3d6356f30675fbb3ecb739f5dc8893de8dc8644
panos1998 <petridisp@ece.auth.gr>
2024-04-25 00:23:24 +0300

    first branch

--- my-app/src/app/chat-component/chat-component.component.ts
+++ my-app/src/app/chat-component/chat-component.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { PromptComponentComponent } from '../prompt-component/prompt-component.component'
 import { MessageThreadContainerComponent } from '../message-thread-container/message-thread-container.component'
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SelectItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 
 @Component({
   selector: 'app-chat-component',
   standalone: true,
-  imports: [CardModule, PromptComponentComponent,MessageThreadContainerComponent],
+  imports: [CardModule, PromptComponentComponent,MessageThreadContainerComponent, SelectButtonModule, ButtonModule],
   templateUrl: './chat-component.component.html',
   styleUrl: './chat-component.component.css'
 })
 export class ChatComponentComponent {
+  currentOptions: SelectItem[];
+  constructor(){
+    this.currentOptions = [];
+    this.currentOptions.push({label:'New York', value:'New Yorke'});
+    this.currentOptions.push({label:'Rome', value:'Rome'});
+    this.currentOptions.push({label:'London', value:'London'});
+    this.currentOptions.push({label:'Istanbul', value:'Istanbul'});
+  }
+ 
 
 }
