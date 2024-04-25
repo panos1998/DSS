909daeb34aa06f8bbd8e4d1cc3cccc29ef132973
panos1998 <petridisp@ece.auth.gr>
2024-04-26 00:18:04 +0300

    messages mockup

--- my-app/src/app/message-thread-container/message-thread-container.component.ts
+++ my-app/src/app/message-thread-container/message-thread-container.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MessageComponentComponent } from '../message-component/message-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-message-thread-container',
   standalone: true,
-  imports: [],
+  imports: [MessageComponentComponent, CommonModule],
   templateUrl: './message-thread-container.component.html',
   styleUrl: './message-thread-container.component.css'
 })
